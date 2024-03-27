989b255d4004ec62d9dd880978b31303dab937a7
erzams <151635854+erzams@users.noreply.github.com>
2024-03-27 12:47:57 +0200

    ...

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
-    
+    return render_template("index.html")
 
+@app.route("/text")
+def text():
+    return render_template("text.html")
+    
 if __name__ == "__main__":
     app.run(port = 5000)
 
